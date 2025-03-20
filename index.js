7d27d252a4bbfdf03f625b8511178c2af4294aa0
aishwarya-9patil <aishwarayagouda@gmail.com>
2025-03-20 13:08:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var http = require('http');
 
+// Create server and add a callback to check the status
 http.createServer(function (req, res) {
-  console.log("Received request");
+  console.log("Received a request"); // Log when a request is received
   res.write('Hello world');
   res.end();
 }).listen(3000, () => {
-  console.log("Server running at http://0.0.0.0:3000/");
+  console.log("Server is running at http://0.0.0.0:3000/");
 });
+
 
