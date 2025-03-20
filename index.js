a49285dd3ab43039d0503bea9f3b80e0f4b685b6
aishwarya-9patil <aishwarayagouda@gmail.com>
2025-03-20 12:57:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('A Monk in Cloud'); //write a response to the client
+  res.write('Hello world'); //write a response to the client
   res.end(); //end the response
 }).listen(3000); // Change the port from 80 to 3000
